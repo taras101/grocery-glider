bef41763ddcbd56cb828081be616f48768a5af27
Taras101 <taras101@gmail.com>
2014-11-11 13:55:50 -0500

    added security and sorting

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -6,8 +6,14 @@ Template.postSubmit.events({
       url: $(e.target).find('[name=url]').val(),
       title: $(e.target).find('[name=title]').val()
     };
-
-    post._id = Posts.insert(post);
-    Router.go('postPage', post);
+  	Meteor.call('postInsert', post, function(error, result) {
+      // display the error to the user and abort
+      if (error)
+        return alert(error.reason);
+      // show this result but route anyway
+      if (result.postExists)
+        alert('This link has already been posted');
+      Router.go('postPage', {_id: result._id});
+    });
   }
 });
